5e89378cb3851120a5c3a23bcdf15ebd84241abd
Aryan Malpotra <aryan.malpotra@originscale.io>
2024-07-28 14:25:24 +0530

    add

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ const taskRoutes = require('./routes/task');
 
 dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+var corsOptions = {
+    origin: '*',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
 
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions))
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
